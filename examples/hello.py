7b1543f6f1d5f7a4e99a940cce998fa2f07bde45
201912219 <83828140+201912219@users.noreply.github.com>
2026-02-24 18:08:05 +0800

    尝试修改
    修改了print内容

--- examples/hello.py
+++ examples/hello.py
@@ -1,9 +1,6 @@
-# 这是一个简单的 Python 示例文件
-# 用于练习 Git 操作
-
 def say_hello(name):
     """向某人问好"""
-    return f"你好, {name}！欢迎学习 GitHub！"
+    return f"你好, {name}！我在尝试使用github"
 
 def main():
     print(say_hello("朋友"))
